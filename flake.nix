0a6762e2263e86f0c4962d5e3df079072c4bedd5
Theodor Tonum <theodor@tonum.no>
2026-03-25 09:37:20 +0100

    feat: add Nix package definition exposing the tdd-guard CLI

--- flake.nix
+++ flake.nix
@@ -17,6 +17,19 @@
       rubyVersion = nixpkgs.lib.fileContents ./.ruby-version;
       ruby = nixpkgs-ruby.lib.mkRuby {inherit pkgs rubyVersion;};
     in {
+      packages.default = pkgs.buildNpmPackage {
+        pname = "tdd-guard";
+        version = "1.1.0";
+        src = ./.;
+        npmDepsHash = "sha256-NJzHv2GwmTrrgfsJmjMsno3PUo1yAgLIH12cVC6yTk4=";
+        nodejs = pkgs.nodejs_22;
+        npmBuildScript = "build";
+        # Workspace symlinks point to reporter source dirs not present in the output
+        postFixup = ''
+          find $out -type l ! -exec test -e {} \; -delete
+        '';
+      };
+
       devShell = with pkgs;
         mkShell {
           buildInputs = [
